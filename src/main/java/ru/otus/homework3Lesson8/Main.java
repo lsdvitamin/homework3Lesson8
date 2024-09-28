734a675e07b29bf5448c4ce86255ebd6565b9134
lsdvitamin <lsdvitamin7@gmail.com>
2024-09-28 23:28:26 +0300

    Реализовал необходимый минимум по ДЗ-3

---
+++ src/main/java/ru/otus/homework3Lesson8/Main.java
@@ -0,0 +1,44 @@
+package ru.otus.homework3Lesson8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Sergei on 28.09.2024 19:26.
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Task task1 = new Task(1, "Eating", Task.Status.CLOSED.toString());
+        Task task2 = new Task(2, "Init", Task.Status.OPEN.toString());
+        Task task3 = new Task(3, "Run", Task.Status.WORKING.toString());
+        Task task4 = new Task(4, "Hiking", Task.Status.CLOSED.toString());
+        Task task5 = new Task(5, "Jogging", Task.Status.CLOSED.toString());
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
+        tasks.add(task4);
+        tasks.add(task5);
+
+        List<Task> taskWorking = tasks.stream()
+                .filter(task -> task.status.equals(Task.Status.WORKING)).toList();
+
+        Integer cntJogging = Math.toIntExact(tasks.stream()
+                .filter(task -> task.status.equals(Task.Status.CLOSED)).count());
+
+        Boolean exists2AndNotExists99 =
+                tasks.stream().noneMatch(task -> task.id == 99) &&
+                        tasks.stream().anyMatch(task -> task.id == 2);
+
+
+        List<Task> taskSorted = tasks.stream()
+                .sorted((t1, t2) -> t1.status.compareTo(t2.status)).
+                collect(Collectors.toList());
+
+        System.out.println();
+    }
+}
---
+++ src/main/java/ru/otus/homework3Lesson8/Task.java
@@ -0,0 +1,39 @@
+package ru.otus.homework3Lesson8;
+
+/**
+ * @author Sergei on 28.09.2024 19:30.
+ * @project homework3Lesson8
+ */
+public class Task implements Comparable<Task> {
+    Integer id;
+    String name;
+
+    @Override
+    public int compareTo(Task task) {
+        if ((task.status.equals(Task.Status.OPEN) && (this.status.equals(Task.Status.WORKING) || this.status.equals(Task.Status.CLOSED))) ||
+                (task.status.equals(Task.Status.WORKING) && (this.status.equals(Task.Status.CLOSED)))) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public enum Status {
+        OPEN("Открыта"),
+        WORKING("В работе"),
+        CLOSED("Закрыта");
+
+        final private String value;
+
+        private Status(String s) {
+            value = s;
+        }
+    }
+
+    Status status;
+
+    public Task(Integer id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = Status.valueOf(status);
+    }
+}
